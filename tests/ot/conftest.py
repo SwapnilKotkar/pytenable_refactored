3447d8c2bbd06ef320537a340bb1de8974aa417a
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2021-09-09 22:13:33 +0530

    commit

---
+++ tests/ot/conftest.py
@@ -0,0 +1,29 @@
+'''
+conftest
+'''
+import pytest
+import responses
+
+from tenable.ot import TenableOT
+from tenable.version import version
+
+
+@pytest.fixture
+@responses.activate
+def fixture_ot():
+    '''fixture ot'''
+    responses.add(
+        method='GET',
+        url='https://localhost:443/v1/version',
+        json={
+            'Module': 'Cobex',
+            'Version': '3.7.0',
+            'NvdVersion': '1.12.0'
+        }
+    )
+    return TenableOT(
+        address='localhost',
+        vendor='pytest',
+        product='pytenable-automated-testing',
+        build=version
+    )
